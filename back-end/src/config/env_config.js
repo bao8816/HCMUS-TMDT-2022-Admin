606a5b8a023b63513bdc59688fadaa3a291fcfab
bao8816 <nile1087@gmail.com>
2022-08-23 23:03:54 +0700

    fix env_config

--- back-end/src/config/env_config.js
+++ back-end/src/config/env_config.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const MONGO_URI = process.env.MONGO_URI;
 const PORT = process.env.PORT;
+const ADMIN_BASE_URL = process.env.ADMIN_BASE_URL;
 const CDN_CLOUD_NAME = process.env.CDN_CLOUD_NAME;
 const CDN_API_KEY = process.env.CDN_API_KEY;
 const CDN_API_SECRET = process.env.CDN_API_SECRET;
@@ -9,6 +10,7 @@ const CDN_API_SECRET = process.env.CDN_API_SECRET;
 module.exports = { 
     MONGO_URI, 
     PORT,
+    ADMIN_BASE_URL,
     CDN_CLOUD_NAME,
     CDN_API_KEY,
     CDN_API_SECRET 
